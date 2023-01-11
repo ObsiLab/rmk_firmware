964a3a849b467f670b31b92a70b48db8d808c487
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-01-11 15:34:19 +0100

    Update keycodes.rs

--- src/keycodes.rs
+++ src/keycodes.rs
@@ -1,2 +1,3 @@
 // check https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
+// or https://github.com/TeXitoi/keyberon/blob/45b8810e50e87e63adb8629f96778a86affda507/src/key_code.rs
 
